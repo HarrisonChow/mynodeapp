491e86aa13948530bc632e489b6ac7f7d645de6f
Airwoman <airwomanzhy@hotmail.com>
2016-09-15 23:49:51 +1000

    change index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
+// var express = require('express');
+// var router = express.Router();
+//
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+//
+// module.exports = router;
+
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
+var showsMapper = require('../lib/showsMapper');
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('Shows is here!');
+});
+router.post('/', function(req, res, next) {
+  var body = req.body;
+  var response = showsMapper(body.payload);
+  res.send({response: response});
 });
 
 module.exports = router;
