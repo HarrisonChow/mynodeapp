3a2a9dd25151b333c5eb862f7868753973d069d0
Airwoman <airwomanzhy@hotmail.com>
2016-09-19 01:07:42 +1000

    change the file name

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,6 @@
-// var express = require('express');
-// var router = express.Router();
-//
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-//
-// module.exports = router;
-
 var express = require('express');
 var router = express.Router();
 var showsMapper = require('../lib/showsMapper');
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Shows is here!');
 });
